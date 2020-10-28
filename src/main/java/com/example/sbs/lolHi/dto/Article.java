efcca15167390e3713226b7c384a0dc1158ebd05
Mimic-chest <shindong0321@gmail.com>
2020-10-28 22:18:20 +0900

    list, detail에서 regDate Exception 오류 해결!

--- src/main/java/com/example/sbs/lolHi/dto/Article.java
+++ src/main/java/com/example/sbs/lolHi/dto/Article.java
@@ -2,9 +2,11 @@ package com.example.sbs.lolHi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Article {
 	private int id;
 	private int memberId;
